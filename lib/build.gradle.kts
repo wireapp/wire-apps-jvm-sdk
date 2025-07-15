191cbff31ed5107b48b0bbb7888da8fc2fc90402
Luca Rospocher <lucarospocher@gmail.com>
2025-07-15 11:18:13 +0200

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     `java-library`
     id("com.gradleup.shadow") version "9.0.0-beta6"
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
-    id("io.gitlab.arturbosch.detekt") version("1.23.7")
+    id("io.gitlab.arturbosch.detekt") version "1.23.7"
     id("app.cash.sqldelight") version "2.1.0"
     id("com.google.protobuf") version "0.9.5"
 
