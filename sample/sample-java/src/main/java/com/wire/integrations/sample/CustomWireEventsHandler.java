e48547978ae987a3da35d5de2867e338148ef618
baris.alis <baris.alis@wire.com>
2025-08-20 15:49:33 +0200

    feat: re-organize sample implementation directories WPB-19280
    Change the group in sample-java from 'org.example' to 'com.wire.integrations'

--- sample/sample-java/src/main/java/com/wire/integrations/sample/CustomWireEventsHandler.java
+++ sample/sample-java/src/main/java/com/wire/integrations/sample/CustomWireEventsHandler.java
@@ -14,7 +14,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-package org.example;
+package com.wire.integrations.sample;
 
 import com.wire.integrations.jvm.WireEventsHandlerDefault;
 import com.wire.integrations.jvm.model.WireMessage;
