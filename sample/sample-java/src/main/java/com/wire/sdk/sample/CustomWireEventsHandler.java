b14cec93f2feb1debf36a8684b28053ab96982e7
baris.alis <baris.alis@wire.com>
2025-12-03 11:31:18 +0100

    Implement test command handling in java-sample

--- sample/sample-java/src/main/java/com/wire/sdk/sample/CustomWireEventsHandler.java
+++ sample/sample-java/src/main/java/com/wire/sdk/sample/CustomWireEventsHandler.java
@@ -22,6 +22,8 @@ import com.wire.sdk.model.ConversationData;
 import com.wire.sdk.model.ConversationMember;
 import com.wire.sdk.model.QualifiedId;
 import com.wire.sdk.model.WireMessage;
+import com.wire.sdk.sample.testcommand.TestCommand;
+import com.wire.sdk.sample.testcommand.TestCommandHandler;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +32,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public class CustomWireEventsHandler extends WireEventsHandlerDefault {
@@ -38,7 +41,43 @@ public class CustomWireEventsHandler extends WireEventsHandlerDefault {
 
     @Override
     public void onTextMessageReceived(@NotNull WireMessage.Text wireMessage) {
-        logger.info("Text message received. conversationId:{}", wireMessage.conversationId());
+        logger.info("Received Text Message. conversationId:{}", wireMessage.conversationId());
+
+        // ___ Case-1 : Process if the message is a test command
+        final var testCommand = TestCommand.getCommand(wireMessage.text().split(" ")[0]);
+        if (testCommand != null) {
+            new TestCommandHandler(getManager()).handle(testCommand, wireMessage);
+            return;
+        }
+
+        // ___ Case-2 : Send ephemeral message if the received message is ephemeral
+        if (wireMessage.expiresAfterMillis() != null) {
+            sendEphemeralTextMessage(
+                    wireMessage.conversationId(),
+                    "**This is a sample ephemeral message** -- Sent from the Sample-Java Ap");
+            return;
+        }
+
+
+        // ___ Case-3: Normal text message
+
+        // Send read receipt
+        final var readReceipt = WireMessage.Receipt.create(
+                wireMessage.conversationId(),
+                WireMessage.Receipt.Type.READ,
+                List.of(wireMessage.id().toString())
+        );
+        getManager().sendMessage(readReceipt);
+
+
+        // Send reaction
+        final var reaction = WireMessage.Reaction.create(
+                wireMessage.conversationId(),
+                wireMessage.id().toString(),
+                Set.of("🧩", "☕️", "🎉")
+        );
+        getManager().sendMessage(reaction);
+
         sendSimpleReply(wireMessage.conversationId(),
                 "Auto reply: " + wireMessage.text() + " -- Sent from the Sample-Java App",
                 wireMessage);
@@ -55,10 +94,9 @@ public class CustomWireEventsHandler extends WireEventsHandlerDefault {
         logger.info("Conversation deleted. conversationId: {}", conversationId);
     }
 
-
     @Override
     public void onAssetMessageReceived(WireMessage.Asset wireMessage) {
-        logger.info("Asset message received. conversationId: {}", wireMessage.conversationId());
+        logger.info("Received Asset Message. conversationId: {}", wireMessage.conversationId());
         sendSimpleReply(wireMessage.conversationId(),
                 "Received Asset:" + wireMessage.name(),
                 wireMessage);
@@ -67,29 +105,29 @@ public class CustomWireEventsHandler extends WireEventsHandlerDefault {
 
     @Override
     public void onCompositeMessageReceived(WireMessage.Composite wireMessage) {
-        logger.info("Composite message received. conversationId: {}", wireMessage.conversationId());
+        logger.info("Received Composite Message. conversationId: {}", wireMessage.conversationId());
         wireMessage.items().forEach(item -> logger.info(" -> item: " + item));
     }
 
     @Override
     public void onButtonClicked(WireMessage.ButtonAction wireMessage) {
-        logger.info("Button clicked. conversationId: {}", wireMessage.conversationId());
+        super.onButtonClicked(wireMessage);
     }
 
     @Override
     public void onButtonClickConfirmed(WireMessage.ButtonActionConfirmation wireMessage) {
-        logger.info("Button click confirmed. conversationId: {}", wireMessage.conversationId());
+        super.onButtonClickConfirmed(wireMessage);
     }
 
     @Override
     public void onPingReceived(WireMessage.Ping wireMessage) {
-        logger.info("Ping received. conversationId: {}", wireMessage.conversationId());
+        logger.info("Received Ping. conversationId: {}", wireMessage.conversationId());
         sendSimplePing(wireMessage.conversationId());
     }
 
     @Override
     public void onLocationMessageReceived(WireMessage.Location wireMessage) {
-        logger.info("Location message received. conversationId: {}", wireMessage.conversationId());
+        logger.info("Received Location message. conversationId: {}", wireMessage.conversationId());
         final var reply =
                 "Received Location: " + wireMessage.latitude() + ", " + wireMessage.longitude() +
                         "\nname: " + wireMessage.name() +
@@ -100,39 +138,39 @@ public class CustomWireEventsHandler extends WireEventsHandlerDefault {
 
     @Override
     public void onMessageDeleted(WireMessage.Deleted wireMessage) {
-        logger.info("Message deleted. conversationId: {}", wireMessage.conversationId());
-        final var reply = "Message deleted with id: " + wireMessage.messageId();
-        sendSimpleReply(wireMessage.conversationId(), reply, wireMessage);
+        logger.info("Received Message deletion. conversationId: {}", wireMessage.conversationId());
+        final var message = "ℹ️Message deleted with id: " + wireMessage.messageId();
+        sendSimpleTextMessage(wireMessage.conversationId(), message);
     }
 
     @Override
     public void onMessageDelivered(@NotNull WireMessage wireMessage) {
-        logger.info("Message delivered. conversationId: {}", wireMessage.getConversationId());
+        super.onMessageDelivered(wireMessage);
     }
 
     @Override
     public void onTextMessageEdited(WireMessage.TextEdited wireMessage) {
-        logger.info("Text message edited. conversationId: {}", wireMessage.conversationId());
+        super.onTextMessageEdited(wireMessage);
     }
 
     @Override
     public void onCompositeMessageEdited(WireMessage.CompositeEdited wireMessage) {
-        logger.info("Composite message edited. conversationId: {}", wireMessage.conversationId());
+        super.onCompositeMessageEdited(wireMessage);
     }
 
     @Override
     public void onMessageReactionReceived(WireMessage.Reaction wireMessage) {
-        logger.info("Reaction received. conversationId: {}", wireMessage.getConversationId());
+        super.onMessageReactionReceived(wireMessage);
     }
 
     @Override
     public void onInCallReactionReceived(WireMessage.InCallEmoji wireMessage) {
-        logger.info("In-call emoji reaction received. conversationId: {}", wireMessage.getConversationId());
+        super.onInCallReactionReceived(wireMessage);
     }
 
     @Override
     public void onInCallHandRaiseReceived(WireMessage.InCallHandRaise wireMessage) {
-        logger.info("In-call hand raise received. conversationId: {}", wireMessage.getConversationId());
+        super.onInCallHandRaiseReceived(wireMessage);
     }
 
     @Override
@@ -150,7 +188,7 @@ public class CustomWireEventsHandler extends WireEventsHandlerDefault {
 
     @Override
     public void onUserLeftConversation(@NotNull QualifiedId conversationId, @NotNull List<QualifiedId> members) {
-        logger.info("User left conversation. conversationId: {}, membersCount: {}", conversationId, members.size());
+        super.onUserLeftConversation(conversationId, members);
     }
 
 
@@ -209,6 +247,18 @@ public class CustomWireEventsHandler extends WireEventsHandlerDefault {
 
         getManager().sendMessage(message);
         logger.info("Message sent. conversationId: {}", conversationId);
+    }
+
+    private void sendEphemeralTextMessage(QualifiedId conversationId, String messageText) {
+        final WireMessage message = WireMessage.Text.create(
+                conversationId,
+                messageText,
+                List.of(),
+                List.of(),
+                10_000L); // Expires after 10 seconds
+
+        getManager().sendMessage(message);
+        logger.info("Ephemeral message sent. conversationId: {}", conversationId);
     }
 
     private void sendSimpleReply(QualifiedId conversationId, String messageText, WireMessage inReplyTo) {
