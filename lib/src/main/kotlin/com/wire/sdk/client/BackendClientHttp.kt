15397cde3915cb04518a655c35f6912ad422b6ce
MarianKijewski <mariankijewski@outlook.com>
2026-02-26 17:55:34 +0100

    Remove the refresh logic from BE client

--- lib/src/main/kotlin/com/wire/sdk/client/BackendClientHttp.kt
+++ lib/src/main/kotlin/com/wire/sdk/client/BackendClientHttp.kt
@@ -18,6 +18,7 @@ package com.wire.sdk.client
 
 import com.wire.sdk.client.BackendClient.Companion.API_VERSION
 import com.wire.sdk.config.IsolatedKoinContext
+import com.wire.sdk.config.getAccessToken
 import com.wire.sdk.exception.WireException
 import com.wire.sdk.model.CryptoClientId
 import com.wire.sdk.model.QualifiedId
@@ -52,7 +53,6 @@ import com.wire.sdk.utils.Mls
 import com.wire.sdk.utils.obfuscateId
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.websocket.DefaultClientWebSocketSession
 import io.ktor.client.plugins.websocket.wss
 import io.ktor.client.request.accept
@@ -64,15 +64,10 @@ import io.ktor.client.request.post
 import io.ktor.client.request.prepareGet
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
-import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.readRawBytes
 import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.OutgoingContent
 import io.ktor.http.contentType
-import io.ktor.http.headers
-import io.ktor.http.setCookie
 import io.ktor.util.encodeBase64
 import io.ktor.websocket.CloseReason
 import io.ktor.websocket.close
@@ -105,10 +100,10 @@ internal class BackendClientHttp(
         handleFrames: suspend (DefaultClientWebSocketSession) -> Unit
     ) {
         logger.info("Connecting to the webSocket, waiting for events")
-        val token = loginUser()
 
+        val bearerToken = getAccessToken(httpClient, appStorage)
         val path = "/await" +
-            "?$ACCESS_TOKEN_QUERY_KEY=$token" +
+            "?$ACCESS_TOKEN_QUERY_KEY=$bearerToken" +
             (appStorage.getDeviceId()?.let { "&$CLIENT_QUERY_KEY=$it" } ?: "")
 
         httpClient.wss(
@@ -147,11 +142,7 @@ internal class BackendClientHttp(
     override suspend fun getApplicationFeatures(): FeaturesResponse {
         logger.info("Fetching application enabled features")
         return cachedFeatures ?: run {
-            val token = loginUser()
             httpClient.get("/$API_VERSION/feature-configs") {
-                headers {
-                    append(HttpHeaders.Authorization, "Bearer $token")
-                }
             }.body<FeaturesResponse>().also { cachedFeatures = it }
         }
     }
@@ -160,96 +151,12 @@ internal class BackendClientHttp(
         logger.info("Confirming team invite")
     }
 
-    private var tokenTimestamp: Long? = null
-
-    private suspend fun loginUser(): String {
-        val currentTime = System.currentTimeMillis()
-
-        // Check if token is valid (not null and not expired)
-        if (cachedAccessToken != null && tokenTimestamp != null) {
-            val timeSinceTokenIssued = currentTime - tokenTimestamp!!
-            if (timeSinceTokenIssued < TOKEN_EXPIRATION_MS) {
-                return cachedAccessToken as String
-            }
-            // Token has expired, will get a new one
-            logger.info("Access token expired, getting a new one")
-        }
-
-        val apiToken = appStorage.getBackendCookie()
-
-        return apiToken?.let {
-            getAccessToken(
-                apiToken = apiToken,
-                currentTime = currentTime
-            )
-        } ?: throw WireException.MissingParameter(
-            message = "apiToken is empty or null."
-        )
-    }
-
-    private suspend fun getAccessToken(
-        apiToken: String,
-        currentTime: Long
-    ): String {
-        val deviceId = appStorage.getDeviceId()
-        val url = "/$API_VERSION/access".let {
-            if (deviceId != null) "$it?client_id=$deviceId" else it
-        }
-        val accessResponse = try {
-            httpClient.post(url) {
-                headers {
-                    append(HttpHeaders.Cookie, "zuid=$apiToken")
-                }
-                accept(ContentType.Application.Json)
-            }
-        } catch (ex: WireException.ClientError) {
-            logger.error("Unable to retrieve access token, Error: ${ex.message}")
-            if (isCookieExpired(ex)) {
-                appStorage.deleteBackendCookie()
-            }
-            // Can't recover from this, need to restart the app with a valid api token
-            throw Error("Current cookie/api-token is expired. Get a apiToken and restart the App")
-        }
-
-        // Chance of cookie renewal -> Store new cookie
-        val responseCookies = accessResponse.setCookie()
-        if (!responseCookies.isEmpty()) {
-            val newCookie = responseCookies.firstOrNull { it.name == "zuid" }?.value
-            if (!newCookie.isNullOrBlank()) {
-                appStorage.saveBackendCookie(newCookie)
-                logger.info("Received new api token from backend, updated stored cookie")
-            }
-        }
-
-        val accessResponseData = accessResponse.body<LoginResponse>()
-
-        // Store accessToken only if deviceId is available, because only in that case we have
-        //  a token with full permissions.
-        deviceId?.let {
-            cachedAccessToken = accessResponseData.accessToken
-            tokenTimestamp = currentTime
-        }
-
-        return accessResponseData.accessToken
-    }
-
-    private fun isCookieExpired(ex: WireException.ClientError): Boolean =
-        ex.throwable is ClientRequestException &&
-            (
-                ex.throwable.response.status == HttpStatusCode.Unauthorized ||
-                ex.throwable.response.status == HttpStatusCode.Forbidden
-            )
-
     override suspend fun updateClientWithMlsPublicKey(
         cryptoClientId: CryptoClientId,
         mlsPublicKeys: MlsPublicKeys
     ) {
-        val token = loginUser()
         try {
             httpClient.put("/$API_VERSION/clients/${appStorage.getDeviceId()}") {
-                headers {
-                    append(HttpHeaders.Authorization, "Bearer $token")
-                }
                 setBody(ClientUpdateRequest(mlsPublicKeys = mlsPublicKeys))
                 contentType(ContentType.Application.Json)
             }
@@ -262,11 +169,7 @@ internal class BackendClientHttp(
     override suspend fun registerClient(
         registerClientRequest: RegisterClientRequest
     ): RegisterClientResponse {
-        val token = loginUser()
         return httpClient.post("/$API_VERSION/clients") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(registerClientRequest)
             contentType(ContentType.Application.Json)
         }.body<RegisterClientResponse>()
@@ -276,14 +179,10 @@ internal class BackendClientHttp(
         cryptoClientId: CryptoClientId,
         mlsKeyPackages: List<ByteArray>
     ) {
-        val token = loginUser()
         val mlsKeyPackageRequest =
             MlsKeyPackageRequest(mlsKeyPackages.map { Base64.getEncoder().encodeToString(it) })
         try {
             httpClient.post("/$API_VERSION/mls/key-packages/self/${appStorage.getDeviceId()}") {
-                headers {
-                    append(HttpHeaders.Authorization, "Bearer $token")
-                }
                 setBody(mlsKeyPackageRequest)
                 contentType(ContentType.Application.Json)
             }
@@ -297,12 +196,8 @@ internal class BackendClientHttp(
         user: QualifiedId,
         cipherSuite: String
     ): ClaimedKeyPackageList {
-        val token = loginUser()
         val url = "$API_VERSION/mls/key-packages/claim/${user.domain}/${user.id}"
         return httpClient.post(url) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             parameter("ciphersuite", cipherSuite)
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
@@ -310,33 +205,21 @@ internal class BackendClientHttp(
     }
 
     override suspend fun getPublicKeys(): MlsPublicKeysResponse {
-        val token = loginUser()
         return httpClient.get("$API_VERSION/mls/public-keys") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
         }.body<MlsPublicKeysResponse>()
     }
 
     override suspend fun uploadCommitBundle(commitBundle: ByteArray) {
-        val token = loginUser()
         httpClient.post("/$API_VERSION/mls/commit-bundles") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(commitBundle)
             contentType(Mls)
         }
     }
 
     override suspend fun sendMessage(mlsMessage: ByteArray) {
-        val token = loginUser()
         httpClient.post("/$API_VERSION/mls/messages") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(mlsMessage)
             contentType(Mls)
         }
@@ -344,13 +227,9 @@ internal class BackendClientHttp(
 
     override suspend fun getConversation(conversationId: QualifiedId): ConversationResponse {
         logger.info("Fetching conversation: $conversationId")
-        val token = loginUser()
         return httpClient.get(
             "/$API_VERSION/conversations/${conversationId.domain}/${conversationId.id}"
         ) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
         }.body<ConversationResponse>()
     }
 
@@ -362,13 +241,9 @@ internal class BackendClientHttp(
      */
     override suspend fun getUserData(userId: QualifiedId): UserResponse {
         logger.info("Fetching user: $userId")
-        val token = loginUser()
         return httpClient.get(
             "/$API_VERSION/users/${userId.domain}/${userId.id}"
         ) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
         }.body<UserResponse>()
     }
 
@@ -378,25 +253,17 @@ internal class BackendClientHttp(
      * @return [SelfUserResponse]
      */
     override suspend fun getSelfUser(): SelfUserResponse {
-        val token = loginUser()
         return httpClient.get("/$API_VERSION/self") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             accept(ContentType.Application.Json)
         }.body<SelfUserResponse>()
     }
 
     override suspend fun getConversationGroupInfo(conversationId: QualifiedId): ByteArray {
         logger.info("Fetching conversation groupInfo: $conversationId")
-        val token = loginUser()
         return httpClient.get(
             "/$API_VERSION/conversations/${conversationId.domain}/${conversationId.id}" +
                 "/groupinfo"
         ) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             accept(Mls)
         }.body<ByteArray>()
     }
@@ -408,10 +275,8 @@ internal class BackendClientHttp(
     ): ByteArray {
         logger.info("Downloading asset ${assetId.obfuscateId()}")
 
-        val token = loginUser()
         return httpClient.prepareGet("$PATH_PUBLIC_ASSETS_V4/$assetDomain/$assetId") {
             headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
                 if (!assetToken.isNullOrBlank()) {
                     append(HEADER_ASSET_TOKEN, assetToken)
                 }
@@ -428,11 +293,7 @@ internal class BackendClientHttp(
     ): AssetUploadResponse {
         logger.info("Uploading new asset")
 
-        val token = loginUser()
         return httpClient.post(PATH_PUBLIC_ASSETS_V3) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(
                 AssetBody(
                     assetContent = encryptedFile,
@@ -447,11 +308,7 @@ internal class BackendClientHttp(
     override suspend fun createGroupConversation(
         createConversationRequest: CreateConversationRequest
     ): ConversationResponse {
-        val token = loginUser()
         return httpClient.post("/$API_VERSION/conversations") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(createConversationRequest)
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
@@ -461,11 +318,7 @@ internal class BackendClientHttp(
     override suspend fun getOneToOneConversation(
         userId: QualifiedId
     ): OneToOneConversationResponse {
-        val token = loginUser()
         return httpClient.get("/$API_VERSION/one2one-conversations/${userId.domain}/${userId.id}") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
         }.body<OneToOneConversationResponse>()
@@ -476,21 +329,16 @@ internal class BackendClientHttp(
         userId: QualifiedId,
         updateConversationMemberRoleRequest: UpdateConversationMemberRoleRequest
     ) {
-        val token = loginUser()
         val conversationPath = "conversations/${conversationId.domain}/${conversationId.id}"
         val memberPath = "members/${userId.domain}/${userId.id}"
 
         httpClient.put("/$API_VERSION/$conversationPath/$memberPath") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(updateConversationMemberRoleRequest)
             contentType(ContentType.Application.Json)
         }
     }
 
     override suspend fun getConversationIds(): List<QualifiedId> {
-        val token = loginUser()
         val conversationIds: MutableList<QualifiedId> = mutableListOf()
 
         var pagingConfig = ConversationListPaginationConfig(
@@ -501,9 +349,6 @@ internal class BackendClientHttp(
         var hasMorePages: Boolean
         do {
             val listIdsResponse = httpClient.post("/$API_VERSION/conversations/list-ids") {
-                headers {
-                    append(HttpHeaders.Authorization, "Bearer $token")
-                }
                 setBody(pagingConfig)
                 contentType(ContentType.Application.Json)
                 accept(ContentType.Application.Json)
@@ -520,7 +365,6 @@ internal class BackendClientHttp(
     override suspend fun getConversationsById(
         conversationIds: List<QualifiedId>
     ): List<ConversationResponse> {
-        val token = loginUser()
         val conversations: MutableList<ConversationResponse> = mutableListOf()
 
         if (!conversationIds.isEmpty()) {
@@ -538,9 +382,6 @@ internal class BackendClientHttp(
 
                 val conversationsListResponse =
                     httpClient.post("/$API_VERSION/conversations/list") {
-                        headers {
-                            append(HttpHeaders.Authorization, "Bearer $token")
-                        }
                         setBody(conversationIdsRequest)
                         contentType(ContentType.Application.Json)
                         accept(ContentType.Application.Json)
@@ -567,14 +408,10 @@ internal class BackendClientHttp(
             conversationId
         )
 
-        val token = loginUser()
         val path = "/$API_VERSION/conversations/${conversationId.domain}/${conversationId.id}" +
             "/members/${userId.domain}/${userId.id}"
 
         httpClient.delete(path) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
         }
 
         logger.info(
@@ -595,13 +432,9 @@ internal class BackendClientHttp(
             conversationId
         )
 
-        val token = loginUser()
         val path = "/$API_VERSION/teams/${teamId.value}/conversations/${conversationId.id}"
 
         httpClient.delete(path) {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
         }
 
         logger.info(
@@ -612,11 +445,7 @@ internal class BackendClientHttp(
     }
 
     override suspend fun getLastNotification(): EventResponse {
-        val token = loginUser()
         val lastNotification = httpClient.get("notifications/last") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             appStorage.getDeviceId()?.let { parameter(CLIENT_QUERY_KEY, it) }
         }.body<EventResponse>()
 
@@ -627,12 +456,8 @@ internal class BackendClientHttp(
         querySize: Int,
         querySince: String?
     ): NotificationsResponse {
-        val token = loginUser()
         try {
             val notifications = httpClient.get("notifications") {
-                headers {
-                    append(HttpHeaders.Authorization, "Bearer $token")
-                }
                 parameter(SIZE_QUERY_KEY, querySize)
                 appStorage.getDeviceId()?.let { parameter(CLIENT_QUERY_KEY, it) }
                 querySince?.let { parameter(SINCE_QUERY_KEY, it) }
@@ -649,11 +474,7 @@ internal class BackendClientHttp(
     }
 
     override suspend fun getClientsByUserId(userId: QualifiedId): List<UserClientResponse> {
-        val token = loginUser()
         val clients = httpClient.get("/users/${userId.domain}/${userId.id}/clients") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
         }.body<List<UserClientResponse>>()
 
         return clients
@@ -662,11 +483,7 @@ internal class BackendClientHttp(
     override suspend fun getClientsByUserIds(
         userIds: List<QualifiedId>
     ): Map<QualifiedId, List<UserClientResponse>> {
-        val token = loginUser()
         val response = httpClient.post("/users/list-clients") {
-            headers {
-                append(HttpHeaders.Authorization, "Bearer $token")
-            }
             setBody(userIds)
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
@@ -724,7 +541,6 @@ internal class BackendClientHttp(
         const val PATH_PUBLIC_ASSETS_V3 = "assets/v3"
         const val PATH_PUBLIC_ASSETS_V4 = "assets/v4"
         const val HEADER_ASSET_TOKEN = "Asset-Token"
-        const val TOKEN_EXPIRATION_MS = 14 * 60 * 1000 // 14 minutes in milliseconds
         const val CONVERSATION_LIST_IDS_PAGING_SIZE = 100
 
         const val ACCESS_TOKEN_QUERY_KEY = "access_token"
