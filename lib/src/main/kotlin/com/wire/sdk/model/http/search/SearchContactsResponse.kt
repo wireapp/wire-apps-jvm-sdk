cffae223b700faa6a50c16373f23a76507374b6b
baris.alis <baris.alis@wire.com>
2026-05-07 14:19:52 +0200

    Finalize SearchContactsResponse.kt

--- lib/src/main/kotlin/com/wire/sdk/model/http/search/SearchContactsResponse.kt
+++ lib/src/main/kotlin/com/wire/sdk/model/http/search/SearchContactsResponse.kt
@@ -16,22 +16,6 @@
 
 package com.wire.sdk.model.http.search
 
-/*
- * Wire
- * Copyright (C) 2026 Wire Swiss GmbH
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see http://www.gnu.org/licenses/.
- */
-
 import com.wire.sdk.model.QualifiedId
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -49,7 +33,7 @@ data class SearchContactsResponse(
     @SerialName("returned")
     val returned: Long? = null,
     @SerialName("took")
-    val took: Long? = null,
+    val took: Long? = null
 )
 
 @Serializable
@@ -69,4 +53,3 @@ data class ContactDocument(
     @SerialName("type")
     val type: String?
 )
-
